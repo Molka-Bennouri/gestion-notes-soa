9fa7b39d5cb1796f08ef655e87834c52f0a4bfba
Molka-Bennouri <molkabennouri1s5@gmail.com>
2025-11-28 17:53:12 +0100

    Initial commit

---
+++ src/main/java/com/example/gestion/GestionNotesSoaApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionNotesSoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionNotesSoaApplication.class, args);
+	}
+
+}
